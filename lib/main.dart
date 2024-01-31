1847b17cbc5dee5aa2ac21ac32c7e60e8c61a3ec
wassy310 <ryg.wsza@gmail.com>
2024-01-31 09:45:51 +0900

    timer app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-import './pages/feed_page.dart';
-import './pages/my_page.dart';
+import './page/next_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'タイマー'),
     );
   }
 }
@@ -33,31 +34,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-  final _pageWidgets = [
-    const FeedPage(),
-    MyPage(),
-  ];
+  int _minute = 0;
+  int _second = 0;
+  int _millisecond = 0;
+  Timer? _timer;
+  bool _isRunning = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pageWidgets.elementAt(_currentIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'フィード'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'マイページ'),
-        ],
-        currentIndex: _currentIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 100,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '$_minute:$_second:${_millisecond.toString().padLeft(2, '0')}',
+                  style: const TextStyle(fontSize: 64),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                toggleTimer();
+              },
+              child: Text(
+                _isRunning ? 'ストップ' : 'スタート',
+                style: TextStyle(
+                  color: _isRunning ? Colors.red : Colors.green,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                resetTimer();
+              },
+              child: const Text(
+                'リセット',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-  
-  void _onItemTapped(int index) {
+
+  void toggleTimer() {
+    if (_isRunning) {
+      _timer?.cancel();
+    } else {
+      _timer = Timer.periodic(
+        const Duration(microseconds: 1000),
+        (timer) {
+          setState(() {
+            _millisecond++;
+            if (_millisecond >= 1000) {
+              _millisecond = 0;
+              _second++;
+              if (_second >= 60) {
+                _second = 0;
+                _minute++;
+              }
+            }
+          });
+
+          if (_minute == 1 && _second == 0 && _millisecond == 0) {
+            resetTimer();
+
+            Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const NextPage()),
+            );
+          }
+        },
+      );
+    }
     setState(() {
-      _currentIndex = index;
+      _isRunning = !_isRunning;
+    });
+  }
+
+  void resetTimer() {
+    _timer?.cancel();
+
+    setState(() {
+      _minute = 0;
+      _second = 0;
+      _millisecond = 0;
+      _isRunning = false;
     });
   }
 }
